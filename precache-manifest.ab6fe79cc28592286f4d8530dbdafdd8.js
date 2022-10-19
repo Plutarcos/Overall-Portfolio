c9117f75efe9443e421692801de00b796e640f53
Plutarcos <yuridene50@gmail.com>
2022-10-19 16:24:25 -0300

    Updates

---
+++ precache-manifest.ab6fe79cc28592286f4d8530dbdafdd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "009ed1a1254dfbef00b8",
+    "url": "/Overall-Portfolio/static/css/main.0a7b4590.chunk.css"
+  },
+  {
+    "revision": "009ed1a1254dfbef00b8",
+    "url": "/Overall-Portfolio/static/js/main.009ed1a1.chunk.js"
+  },
+  {
+    "revision": "8b2e0dbad64883289fca",
+    "url": "/Overall-Portfolio/static/js/1.8b2e0dba.chunk.js"
+  },
+  {
+    "revision": "5f207d1ba1d973f1f5af",
+    "url": "/Overall-Portfolio/static/js/runtime~main.5f207d1b.js"
+  },
+  {
+    "revision": "8a48a8e7e80b21e897cd77b71f0156a7",
+    "url": "/Overall-Portfolio/index.html"
+  }
+];
