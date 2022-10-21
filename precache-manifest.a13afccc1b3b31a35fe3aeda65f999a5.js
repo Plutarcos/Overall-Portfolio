7175fe523585d8ba416c40a5b2eced21b35b0355
Plutarcos <yuridene50@gmail.com>
2022-10-21 16:14:51 -0300

    Updates

---
+++ precache-manifest.a13afccc1b3b31a35fe3aeda65f999a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "151954391a1768693dab",
+    "url": "/Overall-Portfolio/static/css/main.41c7dc3d.chunk.css"
+  },
+  {
+    "revision": "151954391a1768693dab",
+    "url": "/Overall-Portfolio/static/js/main.15195439.chunk.js"
+  },
+  {
+    "revision": "1e46f12f0668cce8cff6",
+    "url": "/Overall-Portfolio/static/js/1.1e46f12f.chunk.js"
+  },
+  {
+    "revision": "5f207d1ba1d973f1f5af",
+    "url": "/Overall-Portfolio/static/js/runtime~main.5f207d1b.js"
+  },
+  {
+    "revision": "c115af8e4ea330cf90016ffd8b4e6de5",
+    "url": "/Overall-Portfolio/static/media/MilltopLogo.c115af8e.png"
+  },
+  {
+    "revision": "65aef81af2f4bd0f762ec21e04b4107f",
+    "url": "/Overall-Portfolio/index.html"
+  }
+];
