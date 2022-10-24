47ecf4efe1b277d059ff469162bf6da58777623e
Plutarcos <yuridene50@gmail.com>
2022-10-24 18:22:06 -0300

    Updates

--- src/Pages/MainPage/Tabs/AboutMe.jsx
+++ src/Pages/MainPage/Tabs/AboutMe.jsx
@@ -8,12 +8,18 @@ export default function AboutMe() {
         <div className='mainPageTabsContent'>
             <div className='aboutMePage'>
                 <aside>
-                    <img src={MyPic} title="Me at Work, there isn't many pictures of me alone..." width='250px' />
+                    <img src={MyPic} alt="MyPicture" title="Me at Work's restroom..." width='250px' />
                 </aside>
                 <h1>Who am I?</h1>
-                <p>This guy right here on the left. Yep, that's me. I am someone who can't stand in one place, always trying to do as much as I can to create 
-                    and solve problems (as any nerdy kid does).
+                <p>This guy right here on the left. Yep, that's me. I am someone who can't stand in one place,
+                    always trying to do as much as I can to create things and solve problems (as any nerdy kid does).</p>
+                <p>Since i was a little kid, i've always loved to create things (slingshots, drawings, diy toys, etc.)
+                    and as a 2000's child, i grew up in front of a computer. At 8 years old I was testing all I could test
+                    on Windows XP, that's when i found a Game Engine named 'FPS Creator'. Although simple, i was fascinated
+                    with the thought that I could create more than just drawings and audio recordings of myself on my computer,
+                    I could create the most present thing in my life, GAMES!
                 </p>
+                <p>That's how all of this starded, with some awlful games made in FPS Creator.</p>
             </div>
         </div>
     );
