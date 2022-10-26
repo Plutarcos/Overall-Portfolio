d692b46e338614d2a34789f0f1c3ca52e2f14736
Plutarcos <yuridene50@gmail.com>
2022-10-26 14:49:39 -0300

    bug fixes

--- src/Pages/MainPage/Tabs/AboutMe.jsx
+++ src/Pages/MainPage/Tabs/AboutMe.jsx
@@ -1,46 +1,49 @@
 import React from 'react';
 import './MainPageTabs.css';
 
-export default function AboutMe() {
 
+export default function AboutMe() {
+    
     return (
         <div id='defaultCSS' className='mainPageTabsContent'>
             <div className='aboutMePage'>
                 <h2>PUT MOUSE HERE</h2>
+
                 <h1 className='crawlTitle'>who am i?</h1>
                 <div className='aboutMeText'>
                     <h1>Yuri de França!</h1>
 
-                    <p>i am someone who can't stand in one place,
-                        always trying to do as much as i can to create things and solve problems (as any nerdy kid does).</p>
+                    <p>I am someone who can't stand in one place,
+                        always trying to do as much as I can to create things and solve problems (as any nerdy kid does).</p>
 
-                    <p>Since i was a little kid, i've always loved to create things (slingshots, drawings, DIY toys, etc.)
-                        and as a 2000's child, i grew up in front of a computer. At 8 years old i was testing all i could test
-                        on Windows xp, that's when i found a Game Engine named 'FPS Creator'. Although simple, i was fascinated
-                        with the thought that i could create more than just drawings and audio recordings of myself on my computer,
-                        i could create the most present thing in my life, GAMES!</p>
+                    <p>Since I was a little kid, I've always loved to create things (slingshots, drawings, DIY toys, etc.)
+                        and as a 2000's child, I grew up in front of a computer. At 8 years old I was testing all I could test
+                        on Windows xp, that's when I found a Game Engine named 'FPS Creator'. Although simple, I was fascinated
+                        with the thought that I could create more than just drawings and audio recordings of myself on my computer,
+                        I could create the most present thing in my life, GAMES!</p>
 
-                    <p>That's how all of this starded, with some awlful games made in FPS Creator. in 2010 i've met
-                        RPG Maker, another Game Engine which provides tools to develop JRPG games. A few years later
-                        i got a new computer which could run Unity Engine, and that was the moment i fell into the
+                    <p>That's how all of this starded, with some awlful games made in FPS Creator. In 2010 I've met
+                        RPG Maker, another Game Engine which provides tools to develop JRPG games. A few years later,
+                        I got a new computer which could run Unity Engine, and that was the moment I fell into the
                         programming world.</p>
 
-                    <p>With Unity Engine, i've learned the basics of JavaScript and C# (At least enought to make
-                        a few 2D platform games, FPS, TPS, i even got the chance to develop a Mobile VR game in which
-                        the player had to avoid obstacles on a road. i was 14 years old at the time and at the peak
-                        of my creativity, so i used all the tools i could found to create.
+                    <p>With Unity Engine, I've learned the basics of JavaScript and C# (At least enought to make
+                        a few 2D platform games, FPS, TPS, I even got the chance to develop a Mobile VR game in which
+                        the player had to avoid obstacles on a road. I was 14 years old at the time and at the peak
+                        of my creativity, so I used all the tools I could found to create.
                     </p>
 
-                    <p>i needed artwork for the games, so i've learned Photoshop. Then i needed publicity videos
-                        to share my games on youtube and groups, so i started to learn video editing (with Vegas Pro and Adobe Premiere).
-                        The music and sound effect for my games i made with some instruments and audio editing and mixing. The more
-                        i used in my games, the more i needed to learn new things, and that's how i got into web developing too.
+                    <p>I needed artwork for the games, so I've learned Photoshop. Then I needed publicity videos
+                        to share my games on youtube and groups, so I started to learn video editing (with Vegas Pro and Adobe Premiere).
+                        The music and sound effect for my games I made with some instruments and audio editing and mixing. The more
+                        I used in my games, the more I needed to learn new things, and that's how I got into web developing too.
                     </p>
 
-                    <p>in 2020, after finishing my System Analysis an Development course, i created MilltopLabs.
-                        it was important to me, although it isn't a proper company yet, i finally got courage to
-                        start developing games for other people rather than my family and friends.
+                    <p>In 2020, after finishing my System Analysis and Development course, I created MilltopLabs.
+                        it was important to me, although it isn't a proper company yet, I finally got courage to
+                        start developing games for other people rather than just my family and friends.
                     </p>
+
                 </div>
             </div>
         </div>
